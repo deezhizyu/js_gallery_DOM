78bb1ac765a2ae77344998b076af42771312607a
deezhizyu <kalenskuja@gmail.com>
2024-06-14 23:14:54 +0200

    make build

---
+++ index.1498f4dd.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery"),r=e.querySelector(".gallery__large-img");e.addEventListener("click",function(e){e.preventDefault();var t=e.target.closest(".list-item__link");t&&(r.src=t.href)});
+//# sourceMappingURL=index.1498f4dd.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const gallery = document.querySelector(".gallery");
+const mainImage = gallery.querySelector(".gallery__large-img");
+// eslint-disable-next-line no-shadow
+gallery.addEventListener("click", (event)=>{
+    event.preventDefault();
+    const link = event.target.closest(".list-item__link");
+    if (!link) return;
+    mainImage.src = link.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
